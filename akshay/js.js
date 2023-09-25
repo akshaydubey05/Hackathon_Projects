6f2f68896d345ca6958bcac6effa9233c2809fcf
Akshay Kumar Dubey <123586645+akshaydubey05@users.noreply.github.com>
2023-09-25 08:26:22 +0530

    Add files via upload

---
+++ akshay/js.js
@@ -0,0 +1,8 @@
+document.getElementById("login-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    // Perform authentication logic here (e.g., checking credentials)
+
+    // For demonstration purposes, always display an alert
+    alert("Login successful!");
+});
