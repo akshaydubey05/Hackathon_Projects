4c7aa1cee81d60b72e14ecdf03ff523608510bc8
Akshay Kumar Dubey <123586645+akshaydubey05@users.noreply.github.com>
2023-09-25 11:31:19 +0530

    Delete js.js

--- js.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("login-form").addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    // Perform authentication logic here (e.g., checking credentials)
-
-    // For demonstration purposes, always display an alert
-    alert("Login successful!");
-});
